21819a806662b1e4f1345704720753b138dba2da
975261413@qq.com <Phone15810813104>
2018-10-25 00:26:46 +0800

    1.test ssh key

--- lesson1/stream.js
+++ lesson1/stream.js
@@ -42,6 +42,7 @@ function WriteStream(){
 //使用pipe Copy文件
 function Copy()
 {
+    
     //一个Readable流和一个Writable流串起来后，所有的数据自动从Readable流进入Writable流，这种操作叫pipe。
     //默认情况下，当Readable流的数据读取完毕，end事件触发后，将自动关闭Writable流。如果我们不希望自动关闭Writable流，需要传入参数 readable.pipe(writable, { end: false });
     var rs=fs.createReadStream('WriteStream.txt','utf-8');
